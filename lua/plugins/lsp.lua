973c0d27baef38d4281a33f48c23b016f7be9039
Shai Cioara <shaicioara@gmail.com>
2024-09-22 17:28:48 +0300

    remove lsp zero and auto completion

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,16 +1,11 @@
 return {
-	'VonHeikemen/lsp-zero.nvim',
+	'neovim/nvim-lspconfig',
 	branch = 'v3.x',
 	dependencies = {
-		{ 'neovim/nvim-lspconfig' },
-		{ 'hrsh7th/cmp-nvim-lsp' },
-		{ 'hrsh7th/nvim-cmp' },
-		{ 'L3MON4D3/LuaSnip' },
 		{ 'Hoffs/omnisharp-extended-lsp.nvim' }
 	},
 
 	config = function()
-		local lsp_zero = require('lsp-zero')
 		local lsp = require('lspconfig')
 
 		lsp.lua_ls.setup({
@@ -58,58 +53,65 @@ return {
 
 		lsp.gopls.setup({})
 
-		lsp_zero.on_attach(function(client, bufnr)
-			-- see :help lsp-zero-keybindings
-			-- to learn the available actions
-			lsp_zero.default_keymaps({ buffer = bufnr })
-			local opts = { buffer = bufnr }
-			local w_desc = function(desc)
-				return { buffer = bufnr, desc = desc }
-			end
+		lsp.fsautocomplete.setup({})
 
-			vim.keymap.set('n', 'gd', vim.lsp.buf.definition, w_desc('[D]efinition'))
-			vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, w_desc('[I]mplementation'))
-			vim.keymap.set('n', 'gr', vim.lsp.buf.references, w_desc('[R]eferences'))
-			vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-			vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, w_desc('[D]eclaration'))
-			vim.keymap.set({ 'n', 'i' }, '<C-k>', vim.lsp.buf.signature_help, opts)
-			vim.keymap.set('n', ']d', vim.diagnostic.goto_next, w_desc('Next [D]iagnostic'))
-			vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, w_desc('Previous [D]iagnostic'))
-			vim.keymap.set('n', '<leader>\'', function() vim.lsp.buf.format { async = true } end,
-				w_desc('Format buffer'))
-			vim.keymap.set(
-				'v', '<leader>\'',
-				function()
-					vim.lsp.buf.format {
-						async = true,
-						range = {
-							['start'] = vim.api.nvim_buf_get_mark(0, '<'),
-							['end'] = vim.api.nvim_buf_get_mark(0, '>')
-						}
-					}
-				end,
-				w_desc('Format buffer')
-			)
-			vim.keymap.set({ 'n' }, '<leader>.', vim.lsp.buf.code_action, w_desc('Code Action'))
-			vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, w_desc('Rename'))
+		vim.api.nvim_create_autocmd("LspAttach", {
+			callback = function(args)
+				local bufnr = args.buf
+				local client = vim.lsp.get_client_by_id(args.data.client_id)
+				if client.server_capabilities.completionProvider then
+					vim.bo[bufnr].omnifunc = "v:lua.vim.lsp.omnifunc"
+				end
+				if client.server_capabilities.definitionProvider then
+					vim.bo[bufnr].tagfunc = "v:lua.vim.lsp.tagfunc"
+				end
 
-			-- Workspaces
-			vim.keymap.set({ 'n' }, '<leader>wa', vim.lsp.buf.add_workspace_folder,
-				{ desc = '[A]dd workspace directory' })
-			vim.keymap.set({ 'n' }, '<leader>wr', vim.lsp.buf.remove_workspace_folder,
-				{ desc = '[R]emove workspace directory' })
-			vim.keymap.set({ 'n' }, '<leader>wl', function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end,
-				{ desc = '[L]ist workspace directory' })
+				local on_lsp_attach = function(client, bufnr)
+					-- see :help lsp-zero-keybindings
+					-- to learn the available actions
+					--lsp_zero.default_keymaps({ buffer = bufnr })
+					local opts = { buffer = bufnr }
+					local w_desc = function(desc)
+						return { buffer = bufnr, desc = desc }
+					end
 
-			vim.api.nvim_create_augroup("LspCommands", {})
+					vim.keymap.set('n', 'gd', vim.lsp.buf.definition, w_desc('[D]efinition'))
+					vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, w_desc('[I]mplementation'))
+					vim.keymap.set('n', 'gr', vim.lsp.buf.references, w_desc('[R]eferences'))
+					vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+					vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, w_desc('[D]eclaration'))
+					vim.keymap.set({ 'n', 'i' }, '<C-k>', vim.lsp.buf.signature_help, opts)
+					vim.keymap.set('n', ']d', vim.diagnostic.goto_next, w_desc('Next [D]iagnostic'))
+					vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, w_desc('Previous [D]iagnostic'))
+					vim.keymap.set('n', '<leader>\'', function() vim.lsp.buf.format { async = true } end,
+						w_desc('Format buffer'))
+					vim.keymap.set(
+						'v', '<leader>\'',
+						function()
+							vim.lsp.buf.format {
+								async = true,
+								range = {
+									['start'] = vim.api.nvim_buf_get_mark(0, '<'),
+									['end'] = vim.api.nvim_buf_get_mark(0, '>')
+								}
+							}
+						end,
+						w_desc('Format buffer')
+					)
+					vim.keymap.set({ 'n' }, '<leader>.', vim.lsp.buf.code_action, w_desc('Code Action'))
+					vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, w_desc('Rename'))
 
-			local k = require("lua.utils")
-			vim.api.nvim_create_autocmd("TextChangedI", {
-				group = "LspCommands",
-				callback = function(args)
-
-				end,
-			})
-		end)
+					-- Workspaces
+					vim.keymap.set({ 'n' }, '<leader>wa', vim.lsp.buf.add_workspace_folder,
+						{ desc = '[A]dd workspace directory' })
+					vim.keymap.set({ 'n' }, '<leader>wr', vim.lsp.buf.remove_workspace_folder,
+						{ desc = '[R]emove workspace directory' })
+					vim.keymap.set({ 'n' }, '<leader>wl',
+						function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end,
+						{ desc = '[L]ist workspace directory' })
+				end
+				on_lsp_attach(client, bufnr)
+			end,
+		})
 	end
 }
