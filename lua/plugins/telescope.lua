474885ab6d6cd2c989699ecdf048f6a5e64354e5
Shai Cioara <shaicioara@gmail.com>
2024-11-19 23:37:37 +0200

    changed git_files to show untracked

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,6 +2,29 @@ return {
 	'nvim-telescope/telescope.nvim',
 	dependencies = { 'nvim-lua/plenary.nvim' },
 	config = function()
+		local function find_command()
+			if 1 == vim.fn.executable("rg") then
+				return { "rg", "--files", "--color", "never", "-g", "!.git" }
+			elseif 1 == vim.fn.executable("fd") then
+				return { "fd", "--type", "f", "--color", "never", "-E", ".git" }
+			elseif 1 == vim.fn.executable("fdfind") then
+				return { "fdfind", "--type", "f", "--color", "never", "-E", ".git" }
+			elseif 1 == vim.fn.executable("find") and vim.fn.has("win32") == 0 then
+				return { "find", ".", "-type", "f" }
+			elseif 1 == vim.fn.executable("where") then
+				return { "where", "/r", ".", "*" }
+			end
+		end
+		require('telescope').setup({
+			pickers = {
+				find_files = {
+					find_command = find_command
+				},
+				git_files = {
+					show_untracked = true
+				}
+			}
+		})
 		local t = require('telescope.builtin')
 		local find_git_files_or_all = function()
 			local ok, _ = pcall(t.git_files)
