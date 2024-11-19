52d7e7e440bad96c368ff1f6db0ec9ccfdf8c62f
Shai Cioara <shaicioara@gmail.com>
2024-11-19 23:39:21 +0200

    add makeprg and a watch mechanism to c# files

---
+++ after/ftplugin/cs.lua
@@ -0,0 +1,30 @@
+local proj_dir = vim.fs.root(0, function(name, _)
+	return name:match('%.csproj$') ~= nil
+end)
+
+local proj_name
+if proj_dir then
+	for entry in vim.fs.dir(proj_dir) do
+		if entry:match('%.csproj') then
+			proj_name = entry
+		end
+	end
+end
+local proj = proj_dir .. "/" .. proj_name
+
+vim.bo.makeprg = "dotnet run " .. proj
+print(proj)
+
+local u = require("terminal_utils")
+local enter_code = vim.api.nvim_replace_termcodes("<CR>", false, false, true)
+vim.api.nvim_create_user_command("Watch",
+	function(args)
+		local watched = proj
+		if args ~= nil and args['args'] then
+			print('hello '..args['args'])
+			watched = args['args']
+		end
+		local win = vim.api.nvim_get_current_win()
+		u.terminal_send_cmd("dotnet watch run --project " .. watched .. enter_code)
+		vim.api.nvim_set_current_win(win)
+	end, { bang = true, nargs = "?" })
---
+++ lua/terminal_utils.lua
@@ -0,0 +1,43 @@
+local M = {}
+M.terminal_send_cmd = function(cmd_text)
+	local function get_first_terminal()
+		local terminal_chans = {}
+		for _, chan in pairs(vim.api.nvim_list_chans()) do
+			if chan["mode"] == "terminal" and chan["pty"] ~= "" then
+				table.insert(terminal_chans, chan)
+			end
+		end
+		table.sort(terminal_chans, function(left, right)
+			return left["buffer"] < right["buffer"]
+		end)
+		if #terminal_chans == 0 then
+			return nil
+		end
+		return terminal_chans[1]["id"]
+	end
+
+	local send_to_terminal = function(terminal_chan, term_cmd_text)
+		vim.api.nvim_chan_send(terminal_chan, term_cmd_text .. "\0")
+	end
+
+	local terminal = get_first_terminal()
+
+	if not terminal then
+		vim.api.nvim_command('vsplit')
+		vim.api.nvim_command('term')
+		terminal = get_first_terminal()
+	end
+
+	if not cmd_text then
+		vim.ui.input({ prompt = "Send to terminal: " }, function(input_cmd_text)
+			if not input_cmd_text then
+				return nil
+			end
+			send_to_terminal(terminal, input_cmd_text)
+		end)
+	else
+		send_to_terminal(terminal, cmd_text)
+	end
+	return true
+end
+return M
