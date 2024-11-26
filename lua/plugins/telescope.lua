8b8fae47a39eeb2924e6fb9d4aafc7db47c14fcc
Shai Cioara <shaicioara@gmail.com>
2024-11-26 12:24:36 +0200

    add fzf to telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,14 @@
 return {
 	'nvim-telescope/telescope.nvim',
-	dependencies = { 'nvim-lua/plenary.nvim' },
+	dependencies = {
+		'nvim-lua/plenary.nvim',
+		{
+			'nvim-telescope/telescope-fzf-native.nvim',
+			build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release'
+		}
+	},
+	opts = {
+	},
 	config = function()
 		local function find_command()
 			if 1 == vim.fn.executable("rg") then
@@ -23,7 +31,17 @@ return {
 				git_files = {
 					show_untracked = true
 				}
+			},
+			extensions = {
+				fzf = {
+					fuzzy = true,   -- false will only do exact matching
+					override_generic_sorter = true, -- override the generic sorter
+					override_file_sorter = true, -- override the file sorter
+					case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+					-- the default case_mode is "smart_case"
+				}
 			}
 		})
+		require('telescope').load_extension('fzf')
 	end
 }
